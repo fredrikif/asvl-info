20cb1a45243d0e2b8ea73de4041cef07f04e540c
fredrikif <fiforsa@gmail.com>
2019-01-22 19:04:48 +0100

    added modules

---
+++ webdev/db.js
@@ -0,0 +1,309 @@
+export const db = [
+  {
+    name: "Aktivitetsplikt",
+    info: [
+      {
+        name: "Helsedirektoratet 2018",
+        url:
+          "https://helsedirektoratet.no/retningslinjer/sykmelderveileder/seksjon?Tittel=aktivitetskravet-erklering-om-aktivitet-10995",
+        description: [
+          "Informasjon fra helsedirektoratet om aktivitetsplikten. ",
+          "Nevner også om unntak fra aktivitetsplikten, og tilrettelegging."
+        ]
+      },
+      {
+        name: "NAVs erfaringer unge mottakere 2018",
+        url:
+          "https://www.nav.no/no/NAV+og+samfunn/Kunnskap/Analyser+fra+NAV/Arbeid+og+velferd/Arbeid+og+velferd/_attachment/544662",
+        description: [
+          "Gjennomgang av NAV-kontorenes erfaringer med aktivitetsplikt for unge sosialhjelpsmottakere",
+          "Kontorene stiller seg positive til aktivitetsplikt og til lovendringen. ",
+          "Kontorene mener brukerne oftere kommer seg ut av passiv tilværelse, ",
+          "og at aktiviteten gir raskere avklaring, samt at de får verdifull kompetanse. ",
+          "Samlet sett ligger andelen sosialhjelpsmottakere i aldersgruppen 18-29 år stabilt, ",
+          "på 2.2 prosent, og rapporten finner ingen nedgang siden lovendringen i 2017. ",
+          "Blant norskfødte i kommuner med en økning i andelen unge med aktivitetsvilkår, ",
+          "finner rapporten indikasjon på en reduksjon i andelen mottakere."
+        ]
+      },
+      {
+        name: "NAVS erfaringer 2018, Klassekampens oppsummering",
+        url: "https://www.klassekampen.no/article/20181214/ARTICLE/181219949",
+        description: [
+          "Kritisk oppsummeringen av rapporten om unge sosialhjelpsmottakere. ",
+          "Fører til økt aktivitet, men ikke nødvendigvis arbeid. ",
+          "Kommer i følge NAV i stor grad ut av passiv tilværelse"
+        ]
+      },
+      {
+        name: "Krav om å stå opp om morra'n, virker det? 2016",
+        url:
+          "https://www.nav.no/systemsider/allePubliserteDokumenter/_attachment/443046?_ts=157d77c92c0&download=true",
+        description: [
+          "Rapporten ser på fire kommuners tidlige implementering, fra 2012 til 2014. ",
+          "I denne undersøkelsen viste obligatorisk oppmøte hver ukedag, med praktiske ",
+          "arbeidsoppgaver, samt strengere sanksjonering ved ugyldig fravær, ",
+          "bedre resultat enn aktivitet som i hovedsak bestod av kurs og veiledning. ",
+          "Rapporten konkluderer med at forskjell på hvordan vilkårene praktiseres er avgjørende ",
+          "for hvilke resultater man oppnår, fra 11-30 prosent bedring, til ingen bedring."
+        ]
+      },
+      {
+        name: "Gudbrandsdølen Dagningen - Ringsaker aktivitetsplikt",
+        url: "asvl/aktivitetsplikt/gd_ringsaker",
+        description: [
+          "Nyhetsartikkel fra GD om aktivitetsplikt i Ringsaker. ",
+          "70 til 85 prosent av deltakerne i Ringsaker har fra 2016 kommet seg ut ",
+          "i jobb og bort fra sosialstønad via 'Arbeidssentralen'. Der jobber brukerne tre dager ",
+          "per uke i ulike etater i kommunen, og i private bedrifter, og den fjerde dagen blir ",
+          "benyttet til kurs, skriving av CV, søknader, og 'dyrking av sosialt fellesskap'."
+        ]
+      }
+    ],
+    studies: [
+      {
+        name: "Welfare Conditionality - Frisch UiO",
+        url: "https://www.frisch.uio.no/docs/PDF/welfare-conditionality.pdf",
+        description: [
+          "Studie gjennomført via UiO, Ragnar Frisch-senteret i 2015. ",
+          "Undersøkte om strengere krav ved aktivitetsplikt førte til større fullføring av skolegang ",
+          "Ved strengere krav for sosialstønad styrkes antall som gjennomfører skolegang, ",
+          "og antallet som blir avhengige av sosialstønad faller. Størst effekt ved ungdom fra ",
+          "ufordelaktig bakgrunn. Kan ikke si nøyaktig hvilken form for aktivitetsplikt som er best, ",
+          "men ser at gjennomføring av en form for aktivitetskrav er viktig - 95% av dataene hadde dette. "
+        ]
+      },
+      {
+        name: "Welfare sanctions and conditionality in the UK",
+        url:
+          "https://www.jrf.org.uk/report/welfare-sanctions-and-conditionality-uk",
+        description: [
+          "Studie fra Storbritannia på effekt og etisk aspekt av aktivitetsplikt (2014). ",
+          "Viser skepsis til etikken rundt at sårbare og yngre personer kan falle utenfor ",
+          "både velferd, i form av økonomisk bistand, og arbeid. "
+        ]
+      },
+      {
+        name:
+          "Final findings report - Welfare conditionality project (UK 2018)",
+        url:
+          "http://www.welfareconditionality.ac.uk/wp-content/uploads/2018/06/40475_Welfare-Conditionality_Report_complete-v3.pdf",
+        description: [
+          "Stor studie fra 2013 til 2018, på aktivitetsplikt i Storbritannia. ",
+          "Viser ineffektivitet til å øke langtidsansettelser, de fleste får kun midlertidige stillinger. ",
+          "Få etablerer fast arbeid og uavhengiget fra sosialstønad. ",
+          "Aktivitetspliktighet gjør lite for å øke motivasjon til å forberede, søke, og oppnå fast arbeid. ",
+          "Viser at det rutinemessig trigger negative personlige, finansielle, helserelaterte, ",
+          "og atferdskonsekvenser, og kan skyve noen bort fra sosialstønad. ",
+          "Mange av tiltakene, som obligatorisk jobbsøkerkurs, blir for generell, og for dårlig, ",
+          "til å kunne hjelpe folk til å komme i fast og betalt arbeid. ",
+          "Etisk vanskelig å fastslå aktivitetsplikt (som gjennomført i Storbritannia), som ",
+          "en god løsning, pga dens ineffektivitet til å folk i fast, lønnet arbeid, som løfter dem ut av fattigdom."
+        ]
+      },
+      {
+        name: "Enforcement of conditional welfare programs Brazil (2017)",
+        url: "http://ftp.iza.org/dp10654.pdf",
+        description: [
+          "Undersøkelse av 'Bolsa Familia', et program som plikter fattige familiers barn til å delta i skolen ",
+          "for å få utbetalt sosialstønad. Virker mest relevant for utviklingsland. ",
+          "Finner at ved gjentatte advarsler, strenge sanksjoner som kollektiv reduksjon i utbetaling, ",
+          "har man god effekt på programmet. Gunstig for familiene som gjennomfører, men potensielt veldig ",
+          "ressurskrevende og vanskelig å gjennomføre for fattige områder. "
+        ]
+      }
+    ]
+  },
+  {
+    name: "Kvalifiseringsprogrammet - KVP",
+    info: [
+      {
+        name: "NAVs informasjon",
+        url:
+          "https://www.nav.no/no/Person/Flere+tema/Sosiale+tjenester/Kvalifiseringsprogrammet#chapter-1",
+        description: ["NAVs informasjon om kvalifiseringsprogrammet"]
+      },
+      {
+        name: "Hvem starter i kvalifiseringsprogrammet, og kommer de i arbeid",
+        url:
+          "https://www.nav.no/no/NAV+og+samfunn/Kunnskap/Analyser+fra+NAV/Arbeid+og+velferd/Arbeid+og+velferd/_attachment/542936?_download=true&_ts=1663e07ce98",
+        description: [
+          "NAVs gjennomgang av KVP fra 2018. Finner positiv utvikling fra 2010 til 2015 ",
+          "på andelen brukere som kommer seg i arbeid, økning fra 19 til 28 prosent. ",
+          "Færre registrert som mottakere av AAP (17 til 12%), og sosialhjelp (50 til 38%). ",
+          "Finner større overgang fra stønad til arbeid for samtlige grupper: over og under 25, ",
+          "norskfødte og ikke-norskfødte (korrelasjon, ikke kausalitet). "
+        ]
+      },
+      {
+        name: "Forslag lovendring KVP (2018)",
+        url:
+          "https://www.regjeringen.no/contentassets/74dbfc177f88466683c910dc6cab95e1/horingsnotat-200318.pdf",
+        description: [
+          "Forslag fra regjeringen om endring av lovverket rundt kvalifiseringsprogrammet. ",
+          "Ønsker løsere rammer rundt deltakelse, samt fjerning av hinder for å delta i nytt ",
+          "kvalifiseringsprogram, dersom vilkårene er oppfylt ved et senere tidspunkt. ",
+          "Større fleksibilitet rundt fravær, ved fravær uten kvalifiseringsstønad, ",
+          "for eksempel ved arbeidsutprøving, kortere fengselsstraff, omsorg av barn. ",
+          "Økte muligheter for gjennomføring av ekstern utdanning, opplæring, og læretid."
+        ]
+      }
+    ],
+    studies: [
+      {
+        name: "SSB - Færre avbryter kvalifiseringsprogrammet",
+        url:
+          "https://www.ssb.no/sosiale-forhold-og-kriminalitet/artikler-og-publikasjoner/faerre-avbryter-kvalifiseringsprogrammet-flere-i-jobb-etterpa",
+        description: [
+          "Oversikt fra SSB om resultater av kvalifiseringsprogrammet. Færre ",
+          "avbryter programmet, flere går til ordinært arbeid etter fullført ",
+          "program, nesten 40 prosent i 2017. Økningen kan skyldes tendens til ",
+          "å rekruttere mer ressurssterke deltakere. Færre totalt sett som går ",
+          "på programmet, fra topp på 13800 i 2010, til 8900 i 2017. ",
+          "Protensiell kritikk: ~'programmet unnlater mange steder å rekruttere ",
+          "gruppene med størst behov', dvs potensiale for mørketall. "
+        ]
+      },
+      {
+        name: "Evaluering av kvalifiseringsprogrammet (2011)",
+        url:
+          "https://evalueringsportalen.no/evaluering/evaluering-av-kvalifiseringsprogrammet-sluttrapport/AFI%20Kvalifiseringsprogr%202011-4.pdf/@@inline",
+        description: [
+          "Forskningsbasert evaluering av kvalifiseringsprogrammet, gjennomført av ",
+          "arbeidsforskningsinstituttet - AFI. KVP bygger på erfaringer med tidligere ",
+          "arbeidsrettete tiltak og satsinger mot samme målgruppe, men ingen av disse ",
+          "har vist mer enn moderate sysselsettingseffekter. Uvisst om KVP vil fungere (2011). ",
+          "Konkluderer at ved riktig tidsbruk, altså ute i virksomheter, slik at tilrettelegging, ",
+          "og individuell oppfølging i ordinært arbeidsliv blir en del av sosialfaglig innsats. "
+        ]
+      },
+      {
+        name: "Helsetilsynet - Kvalifisert til kvalifisering (2015)",
+        url:
+          "https://www.helsetilsynet.no/globalassets/opplastinger/publikasjoner/rapporter2015/helsetilsynetrapport2_2015.pdf",
+        description: [
+          "Rapport om KVP fra helsetilsynet, viser lovbrudd fra NAV i 76 av 92 kommuner (90 NAV-kontor), ",
+          "knyttet til informasjon, rekruttering, innhold i programmet, og oppfølging av deltaker. ",
+          "I nesten halvparten var ikke søkers situasjon godt nok kartlagt ved søknad til KVP. ",
+          "Mangel på individuell tilfredsstillende tilpassing i ca halvparten av kommunene, ",
+          "standardiserte kurs eller tiltak som ikke samsvarer med deltakerens behov. Ofte for lite obligatorisk ",
+          "aktivitet per uke. Problematisk om KVP ikke tilpasses tilstrekkelig, og 2 års-perioden ",
+          "blir brukt opp uten at bruker har fått utbytte, rettigheten til KVP er da oppbrukt, ",
+          "og kan ikke kreves på et senere tidspunkt. "
+        ]
+      }
+    ]
+  },
+  {
+    name: "Lærekandidatordningen",
+    info: [
+      {
+        name: "Utdanningdirektoratets beskrivelse",
+        url:
+          "https://www.udir.no/utdanningslopet/videregaende-opplaring/andre-varianter/larekandidatordning/",
+        description: [
+          "Lærekandidatordningen er for de som ønsker videregående opplæring ",
+          "i bedrift, men som ikke har forutsetninger for å nå kravene til fag- og ",
+          "svenneprøven. Grunnkompetanse som kan oppnås ved individuelt tilpasset opplæring, ",
+          "og kan bygges videre til yrkeskompetanse."
+        ]
+      }
+    ],
+    studies: [
+      {
+        name: "Jeg vet ikke hva alternativet skulle vært",
+        url:
+          "https://brage.bibsys.no/xmlui/bitstream/handle/11250/2500663/NIFUrapport2018-8.pdf?sequence=1&isAllowed=y",
+        description: [
+          "Evaluering av lærekandidatordningen fra NIFU, 2018, bestilt av udir. ",
+          "Fant svært positive holdninger til lærekandidatordningen, både i form av ",
+          "at det er en ordning vi trenger, og må beholde. Variasjon fra 13 til 1.6 ",
+          "prosent lærlinger i ordningen per fylke, hhv Østfold og Oslo. Uklart hvem ",
+          "som faktisk er målgruppen, defineres ulikt mellom fylkeskommunene, noen ",
+          "har eksakte kriterier for hvem som kan bli lærekandidat, andre har generelle ",
+          "retningslinjer, mens noen har ingen retningslinjer i det hele tatt. ",
+          "Uenighet ikke bare blant fylkeskommunene, men også blant LO, NHO, og Utdanningsforbundet ",
+          "om hvem som faktisk er målgruppen. NHO ønsker bred inkludering, mens LO og ",
+          "Utdanningsforbundet ønsker en smalere målgruppe, hvor kun de som ikke kan oppnå ",
+          "fagbrev får innpass i ordningen. Fra 2010, 2011, og 2012 kullene av totalt 1753 ",
+          "lærekandidater, var det per juni 2017 kun 15 som hadde fagbrev, viser dermed at ",
+          "ordningen ikke er en vei til fagbrev. Lærekandidatordningen viser seg å passe best ",
+          "for elever med svake prestasjoner på ungdomsskolen, dårligere resultater for elever ",
+          "med gode karaktergrunnlag fra ungdomsskolen."
+        ]
+      }
+    ]
+  },
+  {
+    name: "Varig tilrettelagt arbeid - VTA",
+    info: [
+      {
+        name: "NAVs beskrivelse",
+        url:
+          "https://www.nav.no/no/Person/Arbeid/Oppfolging+og+tiltak+for+a+komme+i+jobb/Tiltak+for+a+komme+i+jobb/Relatert+innhold/varig-tilrettelagt-arbeid",
+        description: [
+          "NAVs beskrivelse og informasjon om varig tilrettelagt arbeid - VTA. ",
+          "For de med uføretrygd som har behov for arbeid i skjermet virksomhet, ",
+          "kan få tilbud om VTA tilpasset ens behov. "
+        ]
+      },
+      {
+        name: "Regjeringen: Historisk høyt nivå på ordningen med VTA",
+        url:
+          "https://www.regjeringen.no/no/aktuelt/historisk-hoyt-niva-pa-ordningen-med-varig-tilrettelagt-arbeid-vta/id2601088/",
+        description: [
+          "Regjeringen har økt antall plasser for varig tilrettelagt arbeid og var ",
+          "ved mai i 2018 var det totalt over 10.000 uføretrygdede med et slikt tilbud. ",
+          "'Ambisjon om at så mange som mulig skal få delta i arbeidslivet og bruke evnene ",
+          "sine', sa arbeids- og sosialminister Anniken Hauglie. Antallet plasser for VTA ",
+          "har hatt stabil økning de siste årene. "
+        ]
+      }
+    ],
+    studies: [
+      {
+        name: "Behovet for varig tilrettelagt arbeid",
+        url:
+          "http://www.hioa.no/content/download/53400/809899/file/r2012-14.pdf",
+        description: [
+          "Rapport fra 2012, viser at både tiltaksarrangører, kommuner, og NAV, ",
+          "forventer at behovet for VTA vil øke. Forventet økning på 20 til 50 prosent ",
+          "i VTA i skjermet virksomhet de neste 5 årene, altså fram til 2017. I 2010 var ",
+          "antall deltakere i VTA på ~8.700, i 2018 over 10.000. Ønske om flere plasser i ",
+          "både skjermet og ordinær virksomhet. Lavest udekket behov i småbyer / tettsteder. ",
+          "92 prosent var i skjermet virksomhet, og 8 prosent var i ordinær virksomhet. "
+        ]
+      },
+      {
+        name: "De har jo uansett uførepensjon",
+        url:
+          "https://www.arbeidoginkludering.no/siteassets/fafo-rapport-vta.pdf",
+        description: [
+          "Rapport fra Arbeid og Inkludering om varig tilrettelagt arbeid, fra 2008, ",
+          "bestilt av arbeids- og inkluderingsdepartementet. Rapporten går i hovedsak ",
+          "ut på om VTA bidrar til å utvikle deltakernes ressurser gjennom kvalifisering ",
+          "og tilrettelagte oppgaver, om deltakernes muligheter for ordinært arbeid økes, ",
+          "hvordan man kan øke andelen til overgang i ordinært arbeid, og VTA-deltakeres ",
+          "kjennetegn. Godt arbeidsmiljø og lavt sykefravær tyder på et vellykket vedtak. ",
+          "VTA bidrar til å redusere passitivitet og inaktivitet hos deltakerne. ",
+          "Svakhet ved tilrettelegging for den enkelte deltaker, produksjonskrav, ",
+          "finansiering, og manglende oppfølging fra NAV lokalt. "
+        ]
+      },
+      {
+        name:
+          "Sosial bakgrunn, utdanning, arbeid og stønader til personer under 30 år med nedsatt arbeidsevne",
+        url:
+          "https://brage.bibsys.no/xmlui/bitstream/handle/11250/2559147/Sosial%2bbakgrunn%252C%2butdanning%252C%2barbeid%2bog%2bst%25C3%25B8nader.pdf",
+        description: [
+          "Rapport fra 2018 om personer under 30 år med nedsatt arbeidsevne. Per mars 2018 ",
+          "var det ca 190.000 personer i Norge registrert med nedsatt arbeidsevne, hvor 22 prosent ",
+          "av disse var under 30 år. Dvs ca 5 prosent av alle mellom 18 og 29 år. ",
+          "Varig tilrettelagt arbeid som tiltak er det tiltaket hvor brukere i størst grad ",
+          "ender opp på trygd, men som tiltak har VTA en annen og mer varig nedsatt målgruppe ",
+          "enn andre tiltak, som for eksempel arbeidspraksis, oppfølging, eller opplæring. "
+        ]
+      }
+    ]
+  }
+];
